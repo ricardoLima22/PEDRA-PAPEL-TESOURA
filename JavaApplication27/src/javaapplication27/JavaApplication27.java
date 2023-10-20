547baf27264d9fc185d56fcaedb6016efb178697
Ricardo Ls <127208100+ricardoLima22@users.noreply.github.com>
2023-10-20 10:45:43 -0300

    Add files via upload

---
+++ JavaApplication27/src/javaapplication27/JavaApplication27.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication27;
+
+import atividade.*;
+
+public class JavaApplication27 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Jogo game = new Jogo();
+        
+        game.setVisible(true);
+    }
+    
+    
+}
